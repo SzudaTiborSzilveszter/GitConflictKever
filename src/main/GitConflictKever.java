d427f8467ddddaaeca9167e164fdc754e1b89170
SzudaTiborSzilveszter <szudatiborszilveszter@gmail.com>
2026-09-23 15:57:35 +0200

    valasztas hozzaadva

--- src/main/GitConflictKever.java
+++ src/main/GitConflictKever.java
@@ -3,6 +3,7 @@ package main;
 public class GitConflictKever {
 
     public static void main(String[] args) {
+        int valasztas = 1;
         String[] pakli = new String [22];
         String[] regiPakli = pakli.clone();
             switch (valasztas) {        
@@ -15,19 +16,19 @@ public class GitConflictKever {
                 }
              case 2 -> {
                     for (int i = 1; i <= 7; i++) {
-                        ujPakli[i] = pakli[19 - (i - 1) * 3]; 
-                        ujPakli[i + 7] = pakli[20 - (i - 1) * 3];
-                        ujPakli[i + 14] = pakli[21 - (i - 1) * 3]; 
+                        pakli[i] = pakli[19 - (i - 1) * 3]; 
+                        pakli[i + 7] = pakli[20 - (i - 1) * 3];
+                        pakli[i + 14] = pakli[21 - (i - 1) * 3]; 
                     }
                 }
 
                 case 3 -> {
                     for (int i = 1; i <= 7; i++) {
-                        ujPakli[i] = pakli[19 - (i - 1) * 3]; 
-                        ujPakli[i + 7] = pakli[21 - (i - 1) * 3]; 
-                        ujPakli[i + 14] = pakli[20 - (i - 1) * 3]; 
+                        pakli[i] = pakli[19 - (i - 1) * 3]; 
+                        pakli[i + 7] = pakli[21 - (i - 1) * 3]; 
+                        pakli[i + 14] = pakli[20 - (i - 1) * 3]; 
                     }
                 }
         }
-    
+    }
 }
