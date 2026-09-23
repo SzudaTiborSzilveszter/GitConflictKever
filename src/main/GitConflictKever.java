ec8b68153006ea4f115d8bd4252b641381ad3ede
SzudaTiborSzilveszter <szudatiborszilveszter@gmail.com>
2026-09-23 15:45:48 +0200

    switch case fele hozzadva

--- src/main/GitConflictKever.java
+++ src/main/GitConflictKever.java
@@ -3,7 +3,15 @@ package main;
 public class GitConflictKever {
 
     public static void main(String[] args) {
-        
+    String[] regiPakli = pakli.clone();
+        switch (valasztas) {        
+        case 1 -> {
+                for(int i = 1; i <= 7; i++){
+                    pakli[i] = regiPakli[19-(i-1)*3];
+                    pakli[i+7]=regiPakli[20-(i-1)*3];
+                    pakli[i+14]=regiPakli[21-(i-1)*3];
+                }
+            }
     }
     
 }
